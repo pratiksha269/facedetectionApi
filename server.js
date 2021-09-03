8b786f75b1be7d7a2ceccd67ad55a34b02ae780f
pratiksha269 <pratikshalondase@gmail.com>
2021-09-02 16:39:59 -0800

    second commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'postgres'
+  }
+});
+
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+	res.send('Success');
+})
+
+app.post('/signin',(req,res)=> {signin.handleSignin(req,res,db,bcrypt)})
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+app.get('/profile/:id',(req,res) => {signin.handleProfileGet(req,res,db)})
+app.put('/image',(req,res) => {image.handleImage(req,res,db)})
+
+
+app.listen(3000, () => {
+	console.log('server is running');
+})
+
+// /res = 	this is working
+// /signin ---> POST = success/fail
+// /register --> POST = user
+// /profile/:userId --> GET = user 
+// /image --> PUT --> user
